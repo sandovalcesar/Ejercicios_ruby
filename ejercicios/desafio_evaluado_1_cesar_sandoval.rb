f8472afeb7f831a1088103f99044288dd34b711a
Cesar Eduardo Sandoval Soto <csandoval@grupomedicenter.cl>
2023-05-16 12:27:28 -0400

    90% falta hacerlo cirular

--- ejercicios/desafio_evaluado_1_cesar_sandoval.rb
+++ ejercicios/desafio_evaluado_1_cesar_sandoval.rb
@@ -5,20 +5,65 @@ puts ("Ingrese una opcion")
 puts ("1: Piedra")
 puts ("2: Papel")
 puts ("3: Tijeras")
-
+puts(" ")
 v = gets.chomp.to_i
 nr = rand(1..3)
 resultado = 0
+eleccion =" "
+oponente =" "
 
-if v == nr
+
+if (v == nr)
   resultado = 1
-if
+elsif (v==1 && nr==2)
+resultado = 3
+elsif (v==1 && nr==3)
+resultado = 2
+elsif (v==2 && nr==1)
+  resultado = 2
+elsif (v==2 && nr==3)
+  resultado = 1
+elsif (v==3 && nr==1)
+  resultado = 3
+elsif (v==3 && nr==2)
+  resultado = 2
+end
+
+case v
+when 1
+  eleccion="Piedra"
+when 2
+  eleccion="Papel"
+when 3
+  eleccion="tijeras"
+end
+
+case nr
+when 1
+  oponente="Piedra"
+when 2
+  oponente="Papel"
+when 3
+  oponente="Tijeras"
+end
+
+
 
 case resultado
 when 1
-  puts ("Empate")
+  puts ("Empate -_-")
+  puts(" ")
+  puts ("Elegiste #{eleccion}")
+  puts("Tu oponente eligio #{oponente}")
 when 2
-  puts ("Ganaste")
+  puts ("Ganaste ^u^")
+  puts(" ")
+  puts ("Elegiste #{eleccion}")
+  puts("Tu oponente eligio #{oponente}")
 when 3
-  puts ("Perdiste")
+  puts ("Perdiste U_U")
+  puts(" ")
+  puts ("Elegiste #{eleccion}")
+  puts("Tu oponente eligio #{oponente}")
 end
+
