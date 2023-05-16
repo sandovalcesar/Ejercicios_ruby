87b4cf3e6122a7a453cea367605aba5980b41d34
Cesar Eduardo Sandoval Soto <csandoval@grupomedicenter.cl>
2023-05-16 11:27:09 -0400

    avanzando

--- ejercicios/desafio_evaluado_1_cesar_sandoval.rb
+++ ejercicios/desafio_evaluado_1_cesar_sandoval.rb
@@ -1,2 +1,14 @@
-puts ("Bienvenido al juego de Piedra - Papel - Tijeras")
+puts ("-- Bienvenido al juego --")
+puts ("Piedra - Papel - Tijeras")
+gets
+puts ("Ingrese una opcion")
+puts ("1: Piedra")
+puts ("2: Papel")
+puts ("3: Tijeras")
 
+v = gets.to_i
+nr = rand(1..3)
+
+
+puts (v)
+puts (nr)
