523f637d97f3d4b3c984d309832c19c964f8bc83
Cesar Eduardo Sandoval Soto <cesar.sandoval.soto@icloud.com>
2023-05-19 12:38:23 -0400

    sueldo ok

--- ejercicios/paso.rb
+++ ejercicios/paso.rb
@@ -1,13 +1,17 @@
 def aumento_de_sueldo(sueldo , porcentaje)
-  resultado = sueldo * porcentaje
-  puts "su sueldo #{sueldo}, aumentado en un #{porcentaje}% es: #{resultado}"
-  return resultado
+  if sueldo >0
+    porcentaje_formateado = sprintf('%.2f', porcentaje / 100.0)
+    puts porcentaje_formateado
+    resultado = sueldo + (sueldo * porcentaje)
+    puts "su sueldo #{sueldo}, aumentado en un #{porcentaje}% es: #{resultado}"
+    return resultado
   end
+end
 
-puts "Ingrese el sueldo:"
-sueldo = gets.chomp.to_i
-puts "Ingrese el % a aumentar: "
-porcentaje = gets.chomp.to_i
+print "Ingrese el sueldo: $"
+sueldo = gets.chomp.to_f
+print "Ingrese el % a aumentar: "
+porcentaje = gets.chomp.to_f
 aumento_de_sueldo(sueldo, porcentaje)
 
 
