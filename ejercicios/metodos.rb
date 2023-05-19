17e587ea96ab8efb63a0caa5b070b5629d39a468
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-05-18 21:57:18 -0400

    metodos.rb update

--- ejercicios/metodos.rb
+++ ejercicios/metodos.rb
@@ -1,3 +1,4 @@
+=begin
 def fahrenheit_a_celsius(fahrenheit)
   celsius = (fahrenheit + 40) / 1.8 -40
   return celsius
@@ -7,3 +8,16 @@ puts "Ingresa una temperatura en grados fahrenheit: "
 grados_fahrenheit = gets.chomp.to_f
 grados_celsius = fahrenheit_a_celsius(grados_fahrenheit)
 puts "#{grados_fahrenheit} grados Celsius son #{grados_celsius} grados Fahrenheit."
+
+=end
+puts "Ingrese la altura del triangulo"
+altura = gets.chomp.to_i.abs
+
+
+valor_caracter = '#'
+caracter = '#'
+
+for i in 1..altura
+  puts " " * (altura - i) + valor_caracter
+  valor_caracter += caracter * 2
+end
