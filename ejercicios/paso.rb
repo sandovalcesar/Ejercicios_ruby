4a9326910dcc07b3ac6ea4c48a1783f3b51c33f7
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-05-19 17:15:19 -0400

    algo no va bien

--- ejercicios/paso.rb
+++ ejercicios/paso.rb
@@ -20,7 +20,37 @@ end
 
 #juego de igualdad
 def juego(eleccion)
+  n_comp = rand(0..1000)
+  gana = false
+  puts n_comp
+  if n_comp > eleccion
+    puts 'Mas arriba'
+    gana = false
+  end
+  if n_comp < eleccion
+    puts 'mas abajo'
+    gana = false
+  end
+  if n_comp == eleccion
+    puts 'FELICITACIONES'
+    puts "Ganaste, el numero que elegiste es #{eleccion} y el numero que pense fue #{n_comp}"
+    gana = true
+  end
+  return gana
+end
 
+puts 'BIENVENIDO AL JUEGO'
+puts ""
+print 'advina en que numero estoy pensando: '
+eleccion = gets.chomp.to_i
+victoria = juego(eleccion)
+
+if eleccion > 0
+  while victoria == false
+    juego(eleccion)
+  end
+else
+  puts 'error'
 end
 
 
