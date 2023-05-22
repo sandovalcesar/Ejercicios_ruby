18452c9a2744c2ec99df20558908f9b054d43306
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-05-22 09:47:58 -0400

    todo ok

--- ejercicios/paso.rb
+++ ejercicios/paso.rb
@@ -1,4 +1,3 @@
-=begin
 #aumento de sueldo
 def aumento_de_sueldo(sueldo , porcentaje)
     porcentaje_f = porcentaje / 100.0 #da un resultado 0.algo
@@ -16,7 +15,7 @@ if sueldo>0
   aumento_de_sueldo(sueldo, porcentaje)
 else puts "error"
 end
-=end
+
 
 #juego de igualdad
 def juego(eleccion)
@@ -42,8 +41,43 @@ print 'Adivina en qué número estoy pensando: '
 eleccion = gets.chomp.to_i
 
 while juego(eleccion) == false
-  print 'Intenta nuevamente: '
+  print "Intenta nuevamente: "
   eleccion = gets.chomp.to_i
 end
 
 
+#IMC
+def imc(peso, altura)
+imc = peso / altura**2
+return imc
+end
+
+def casos(imc)
+  if imc < 18.5
+    diag = "Bajo peso"
+  elsif imc >= 18.5 && imc <= 24.9
+    diag = "Peso normal"
+  elsif imc >= 25.0 && imc <= 29.9
+    diag = "Sobrepeso"
+  elsif imc >= 30.0 && imc <= 34.9
+    diag = "Obesidad grado 1"
+  elsif imc >= 35.0 && imc <= 39.9
+    diag = "Obesidad grado 2"
+  else
+    diag = "Obesidad grado 3"
+  end
+  return diag
+end
+
+puts "Conozca su Indice de Masa Corporal"
+puts "Ingrese su peso en kg: "
+peso = gets.chomp.to_f
+
+puts "Ingrese su altura en metros: "
+altura = gets.chomp.to_f
+
+imc_calculado = imc(peso, altura)
+diagnostico = casos(imc_calculado)
+
+puts "Su IMC es: #{imc_calculado}"
+puts "Su diagnostico es: #{diagnostico}"
--- pruebas.rb
+++ pruebas.rb
@@ -1,12 +1,37 @@
 
-arreglo = %w{a b c d f g}
+#IMC
+def imc(peso, altura)
+  imc = peso / altura**2
+  return imc
+end
 
+def casos(imc)
+  if imc < 18.5
+    diag = "Bajo peso"
+  elsif imc >= 18.5 && imc <= 24.9
+    diag = "Peso normal"
+  elsif imc >= 25.0 && imc <= 29.9
+    diag = "Sobrepeso"
+  elsif imc >= 30.0 && imc <= 34.9
+    diag = "Obesidad grado 1"
+  elsif imc >= 35.0 && imc <= 39.9
+    diag = "Obesidad grado 2"
+  else
+    diag = "Obesidad grado 3"
+  end
+  return diag
+end
 
-arreglo.insert(6, "a", "i")
+puts "Conozca su Indice de Masa Corporal"
+puts "Ingrese su peso en kg: "
+peso = gets.chomp.to_f
 
-print arreglo.uniq
+puts "Ingrese su altura en metros: "
+altura = gets.chomp.to_f
 
+imc_calculado = imc(peso, altura)
+diagnostico = casos(imc_calculado)
 
-
-
+puts "Su IMC es: #{imc_calculado}"
+puts "Su diagnostico es: #{diagnostico}"
 
