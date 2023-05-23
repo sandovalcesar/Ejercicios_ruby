7b673e680befdde657c295429ca194df084f533f
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-05-22 20:53:29 -0400

    avances en clases

--- ejercicios/arrays.rb
+++ ejercicios/arrays.rb
@@ -15,15 +15,19 @@ end
 print mi_arreglo
 =end
 
-def contar(lista_numeros) #lista es un arreglo
-  repeticiones = 0
-  lista_numeros.each do |e|
-    if e == 10
-      repeticiones += 1
+def contar(lista,num)
+  # lista es un arreglo de 1 x n
+  repeticion = 0
+
+  lista.each do |e|
+    if e == num
+      repeticion += 1
     end
   end
-  repeticiones
+  return repeticion
 end
 
-numeros = [1, 4, 5, 7, 9, 10, 15, 10, 20, 2, 10, 200, 300, 10, 50]
-puts contar(numeros)
+numeros = [1, 4, 5, 7, 9, 10, 15, 10, 50, 20, 2, 10, 200, 300, 10, 50]
+print"Ingresar el nro a contar "
+num = gets.chomp.to_i
+puts contar(numeros,num)
