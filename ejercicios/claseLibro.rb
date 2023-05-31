e6d43a60621e0b9abdbf574d7c61f9a1b436df7c
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-05-31 16:02:55 -0400

    class libro

---
+++ ejercicios/claseLibro.rb
@@ -0,0 +1,25 @@
+class Libro
+  @name = ''
+  @autor = ''
+  @numero_paginas = 0
+  @editial = ''
+
+  def initialize (name, autor, numero_paginas, editorial)
+    @name = name
+    @autor = autor
+    @numero_paginas = numero_paginas
+    @editial = editorial
+  end
+
+  def publicar
+    puts "El libro #{@name} de #{@autor} ha sido publicado"
+  end
+
+  def anunciar (medio:)
+    puts "El libro #{@libro} ha sido publicado en #{@medio}"
+  end
+end
+
+libro_1 = Libro.new('It', 'Stephen King', 322, 'De bolsillo')
+libro_1.publicar
+
